6a8b3dbd2663cea1409c0c460f1724040ef22d1d
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2021-02-01 20:30:47 +0100

    Player class added.

---
+++ Player.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    class Player
+    {
+        private DirectSoundOut output;
+        private AudioFileReader reader;
+
+        public PlaybackState GetPlaybackState => output.PlaybackState;
+
+        public Player(string path, double volume)
+        {
+            reader = new AudioFileReader(path) { Volume = (float) volume };
+            output = new DirectSoundOut(200);
+            output.PlaybackStopped += Output_PlaybackStopped;
+            var wc = new WaveChannel32(reader);
+            wc.PadWithZeroes = false;
+            output.Init(wc);
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == PlaybackState.Playing)
+                {
+                    output.Stop();
+                }
+                output.Dispose();
+                output = null;
+            }
+
+            if (reader != null)
+            {
+                reader.Dispose();
+                reader = null;
+            }
+        }
+
+        public void Play(PlaybackState currentState, double currentVolume)
+        {
+            if ((currentState == PlaybackState.Stopped || currentState == PlaybackState.Paused) && output != null)
+            {
+                output.Play();
+                reader.Volume = (float)currentVolume;
+            }    
+        }
+
+        public void Stop()
+        {
+            if (output != null)
+            {
+                output.Stop();
+            }
+        }
+
+        public void Pause()
+        {
+            if (output != null)
+            {
+                output.Pause();
+            }
+        }
+
+        public void PlayPause(double currenVolume)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == PlaybackState.Playing)
+                {
+                    Pause();
+                }
+                else
+                {
+                    Play(output.PlaybackState, currenVolume);
+                }
+            }
+            else
+            {
+                Play(PlaybackState.Stopped, currenVolume);
+            }
+        }
+
+        public double GetLenght()
+        {
+            if (reader != null)
+            {
+                return reader.TotalTime.TotalSeconds;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double GetPosition()
+        {
+            if (reader != null)
+            {
+                return reader.CurrentTime.TotalSeconds;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void SetPosition(double value)
+        {
+            if (reader != null)
+            {
+                reader.CurrentTime = TimeSpan.FromSeconds(value);
+            }
+        }
+
+        public void SetVolume(float value)
+        {
+            if (reader != null)
+            {
+                reader.Volume = value;
+            }
+        }
+    }
+}
