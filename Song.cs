22a7e6884b23bdb587e2df0553b10b7115d17fc5
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2021-02-02 12:49:31 +0100

    Song class added.

---
+++ Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+
+namespace MusicPlayer
+{
+    class Song
+    {
+        private string path;
+        private string title;
+        private string album;
+        private string[] artists;
+
+        public string GetPath => path;
+        public string GetTitle => title;
+        public string GetAlbum => album;
+        public string[] GetArtists => artists;
+
+        public Song(string path)
+        {
+            var tag = TagLib.File.Create(path);
+            this.path = path;
+            this.title = tag.Tag.Title;
+            this.album = tag.Tag.Album;
+            this.artists = tag.Tag.AlbumArtists;
+        }
+    }
+}
